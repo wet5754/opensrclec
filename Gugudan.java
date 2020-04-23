0f5fb7fd8b7e0d1e52fd7ee6ac046b2681d6c23f
wet5754 <deoksoo5754@gmail.com>
2020-04-23 13:28:46 +0900

    ver 1.5

--- Gugudan.java
+++ Gugudan.java
@@ -9,10 +9,16 @@ public class Gugudan {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("출력할 단을 입력하세요 : ");
 		int dan = scan.nextInt();
-		
-		for(int i =1;i<10;i++) {
-			System.out.println(dan +" * "+i+" = "+(i*dan));
+		if(dan ==0) {
+			for (int i = 2; i < 10; i++) {
+				for (int j = 2; j < 10; j++)
+					System.out.print(i + " * " + j + " = " + (i * j)+" ");
+				System.out.println();
+			}
 		}
+		else
+			for (int i = 1; i < 10; i++)
+				System.out.println(dan + " * " + i + " = " + (i * dan));
 	}
 
 }
